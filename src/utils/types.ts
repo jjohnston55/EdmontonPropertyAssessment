6d92f9705c62ecf0b2fd4c1d74caf4c9ed195a7b
Jeff Johnston <46493038+jjohnston55@users.noreply.github.com>
2021-05-17 11:35:38 -0600

    Created typescript interfaces for API calls

---
+++ src/utils/types.ts
@@ -0,0 +1,24 @@
+export interface Neighbourhood {
+	name: string;
+	neighbourhood_number: number;
+	descriptive_name: string;
+	description?: string;
+	geometry_multipolygon: {
+		coordinates: [];
+		type: string;
+	};
+}
+
+export interface Property {
+	account_namber: string;
+	suite?: string;
+	house_number: string;
+	street_name: string;
+	garage: "Y" | "N";
+	neighbourhood_id: number;
+	neighbourhood: string;
+	ward: string;
+	assessed_value: number;
+	latitude: string;
+	longitude: string;
+}
