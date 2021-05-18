2ed8586aa4c7bc8aa481bb3758299a5fa3c90e5c
Jeff Johnston <46493038+jjohnston55@users.noreply.github.com>
2021-05-18 15:21:40 -0600

    Changed layout styling of app

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import PropertyScreen from "./components/PropertyScreen";
 import { Neighbourhood, Property } from "./utils/types";
 import logoSrc from "./resources/EDM.png";
 
-function App() {
-	const [error, setError] = useState("");
+export default function App() {
+	const [neighbourhoodText, setNeighbourhoodText] = useState("");
 	const [property, setProperty] = useState<Property>();
 	const [properties, setProperties] = useState<Property[]>([]);
 	const [neighbourhoods, setNeighbourhoods] = useState<Neighbourhood[]>([]);
-	const [selectedNeighbourhood, setSelectedNeighbourhood] = useState(0);
+	const [selectedNeighbourhood, setSelectedNeighbourhood] = useState<Neighbourhood>();
 	const [showProperty, setShowProperty] = useState(false);
 
 	useEffect(() => {
@@ -21,29 +21,37 @@ function App() {
 	}, []);
 
 	useEffect(() => {
-		if (selectedNeighbourhood !== 0) {
+		if (selectedNeighbourhood !== undefined) {
 			// https://data.edmonton.ca/City-Administration/Property-Assessment-Data-Current-Calendar-Year-/q7d6-ambg
 			// https://data.edmonton.ca/resource/q7d6-ambg.json = property value
 			fetch(
-				`https://data.edmonton.ca/resource/q7d6-ambg.json?$limit=50000&neighbourhood_id=${selectedNeighbourhood}`
+				`https://data.edmonton.ca/resource/q7d6-ambg.json?$limit=50000&neighbourhood_id=${selectedNeighbourhood.neighbourhood_number}`
 			)
 				.then((resp) => resp.json())
 				.then((data) => setProperties(data));
 		}
 	}, [selectedNeighbourhood]);
 
-	const changeNeighbourhood = (event: ChangeEvent<HTMLInputElement>): void => {
-		const descriptiveName = event.target.value;
+	useEffect(() => {
+		if (neighbourhoodText.length > 0) {
+			const neighbourhood = neighbourhoods.find(
+				(n) => n.descriptive_name === neighbourhoodText
+			);
 
-		const neighbourhood = neighbourhoods.find((n) => n.descriptive_name === descriptiveName);
-
-		if (neighbourhood) {
-			setSelectedNeighbourhood(neighbourhood.neighbourhood_number);
-			setError("");
+			if (neighbourhood) {
+				setSelectedNeighbourhood(neighbourhood);
+			} else {
+				setSelectedNeighbourhood(undefined);
+				setShowProperty(false);
+			}
 		} else {
-			setSelectedNeighbourhood(0);
-			setError("Neighbourhood does not exist");
+			setSelectedNeighbourhood(undefined);
+			setShowProperty(false);
 		}
+	}, [neighbourhoodText]);
+
+	const changeNeighbourhood = (event: ChangeEvent<HTMLInputElement>): void => {
+		setNeighbourhoodText(event.target.value);
 	};
 
 	const changeProperty = (event: ChangeEvent<HTMLInputElement>): void => {
@@ -61,6 +69,7 @@ function App() {
 			if (property) {
 				setProperty(property);
 			} else {
+				setProperty(undefined);
 				setShowProperty(false);
 			}
 		}
@@ -74,54 +83,80 @@ function App() {
 
 	return (
 		<Container>
-			<Row>
-				<Image src={logoSrc} alt="Edmonton.png" />
-			</Row>
-			<Row>
-				<span>{error}</span>
-			</Row>
-			<Row>
-				<Input
-					list="neighbourhoods"
-					placeholder="Enter Neighbourhood Name.."
-					onChange={changeNeighbourhood}
-				/>
-				<datalist id="neighbourhoods">
-					{neighbourhoods.length > 0 &&
-						neighbourhoods.map((neighbourhood) => (
-							<option key={neighbourhood.neighbourhood_number}>
-								{neighbourhood.descriptive_name}
-							</option>
-						))}
-				</datalist>
-			</Row>
+			<Col className={!showProperty ? "fullscreen light" : "light"}>
+				<Row className="top">
+					<Image src={logoSrc} alt="Edmonton.png" />
+				</Row>
+				<Row>
+					<h2>See your assessed property value</h2>
+				</Row>
 
-			{selectedNeighbourhood !== 0 && (
-				<>
-					<Row>
-						<Input
-							list="properties"
-							placeholder="Address.."
-							onChange={changeProperty}
-						/>
-						<datalist id="properties">
-							{properties.length > 0 &&
-								properties.map((property) => (
-									<option
-										key={property.account_number}
-										id={property.account_number}>
-										{property.house_number} {property.street_name}{" "}
-										{property.suite ? `Suite ${property.suite}` : ""}
-									</option>
-								))}
-						</datalist>
-					</Row>
-					<Row>
-						<button onClick={seeProperty}>See Property</button>
-					</Row>
-				</>
+				<Row>
+					<Input
+						list="neighbourhoods"
+						placeholder="Enter Neighbourhood Name.."
+						onChange={changeNeighbourhood}
+						value={neighbourhoodText}
+					/>
+					<datalist id="neighbourhoods">
+						{neighbourhoods.length > 0 &&
+							neighbourhoods.map((neighbourhood) => (
+								<option key={neighbourhood.neighbourhood_number}>
+									{neighbourhood.descriptive_name}
+								</option>
+							))}
+					</datalist>
+				</Row>
+
+				{selectedNeighbourhood && (
+					<>
+						<Row>
+							<Input
+								list="properties"
+								placeholder="Address.."
+								onChange={changeProperty}
+							/>
+							<datalist id="properties">
+								{properties.length > 0 &&
+									properties.map((property) => (
+										<option
+											key={property.account_number}
+											id={property.account_number}>
+											{property.house_number} {property.street_name}{" "}
+											{property.suite ? `Suite ${property.suite}` : ""}
+										</option>
+									))}
+							</datalist>
+						</Row>
+						{!showProperty && (
+							<Row>
+								<Button onClick={seeProperty}>View Property</Button>
+							</Row>
+						)}
+					</>
+				)}
+				<Row>
+					<a
+						href="https://data.edmonton.ca/City-Administration/Property-Assessment-Data-Current-Calendar-Year-/q7d6-ambg"
+						rel="noreferrer noopenner"
+						target="_blank">
+						Edmonton Open Data - Property Assessment
+					</a>
+				</Row>
+				<Row>
+					<a
+						href="https://data.edmonton.ca/City-Administration/City-of-Edmonton-Neighbourhoods/65fr-66s6"
+						rel="noreferrer noopenner"
+						target="_blank">
+						Edmonton Open Data - Neighbourhood List
+					</a>
+				</Row>
+			</Col>
+			{showProperty && property && selectedNeighbourhood && (
+				<Col>
+					<PropertyScreen property={property} neighbourhood={selectedNeighbourhood} />
+				</Col>
 			)}
-			{showProperty && property && <PropertyScreen property={property} />}
 		</Container>
 	);
 }
@@ -130,13 +165,42 @@ const Container = styled.div`
 	align-items: center;
 	color: white;
 	display: flex;
-	flex-direction: column;
+	flex-direction: row;
 	height: 100vh;
-	width: 100vw;
+	width: 100%;
+
+	.light {
+		background-color: #0081bc;
+		width: 40%;
+
+		.top {
+			margin-top: 20vh;
+		}
+	}
+
+	.fullscreen {
+		width: 100%;
+	}
+`;
+
+const Col = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	height: 100%;
+	width: 100%;
 `;
 
 const Row = styled.div`
+	display: flex;
+	justify-content: center;
 	margin: 10px;
+	width: calc(100% - 20px);
+
+	a {
+		display: block;
+		color: white;
+	}
 `;
 
 const Input = styled.input`
@@ -144,6 +208,18 @@ const Input = styled.input`
 	border: 0;
 	border-radius: 10px;
 	font-size: 18px;
+	width: calc(80% - 20px);
+	max-width: 500px;
+`;
+
+const Button = styled.button`
+	width: 10rem;
+	background-color: white;
+	border: 0;
+	border-radius: 10px;
+	font-size: 18px;
+	cursor: pointer;
+	padding: 10px;
 `;
 
 const Image = styled.img`
@@ -151,5 +227,3 @@ const Image = styled.img`
 	width: 30vw;
 	height: auto;
 `;
-
-export default App;
--- src/components/PropertyScreen.tsx
+++ src/components/PropertyScreen.tsx
@@ -1,65 +1,74 @@
 import React from "react";
 import styled from "styled-components";
-import { Property } from "../utils/types";
+import { Neighbourhood, Property } from "../utils/types";
 import Map from "./Map";
 
 type PropertyProps = {
 	property: Property;
+	neighbourhood: Neighbourhood;
 };
 
-export default function PropertyScreen({ property }: PropertyProps) {
+export default function PropertyScreen({ property, neighbourhood }: PropertyProps) {
 	const formatter = new Intl.NumberFormat("en-CA", {
 		currency: "CAD",
 		style: "currency",
 	});
 
 	console.log(property);
+	console.log(neighbourhood);
 	return (
 		<Container>
-			<Col>
-				<h2>Location</h2>
-				<Map
-					latitude={parseFloat(property.latitude)}
-					longitude={parseFloat(property.longitude)}
-				/>
-			</Col>
-			<Col>
-				<div>
+			<Row>
+				<div className="map">
+					<Map
+						latitude={parseFloat(property.latitude)}
+						longitude={parseFloat(property.longitude)}
+					/>
+				</div>
+			</Row>
+			<Row>
+				<Col>
 					<h2>Address</h2>
 					<h1>
 						{property.house_number} {property.street_name}{" "}
 						{property.suite ? `Suite ${property.suite} ` : ""}
 					</h1>
-				</div>
-				<div>
-					<h2>Neighbourhood</h2>
-					<span>{property.neighbourhood}</span>
-				</div>
-				<div>
+				</Col>
+				<Col>
 					<h2>Assessed Value</h2>
-					<span>{formatter.format(property.assessed_value)}</span>
-				</div>
-			</Col>
+					<h3>{formatter.format(property.assessed_value)}</h3>
+				</Col>
+			</Row>
+			<Row>
+				<Col>
+					<h2>{neighbourhood.descriptive_name}</h2>
+					{neighbourhood.description && <span>{neighbourhood.description}</span>}
+				</Col>
+			</Row>
 		</Container>
 	);
 }
 
 const Container = styled.div`
 	display: flex;
-	flex-direction: row;
-	margin: 50px;
-	/* align-items: center; */
+	flex-direction: column;
 	justify-content: center;
+	width: 100%;
+`;
 
-	@media only screen and (max-width: 400px) {
-		flex-direction: column;
+const Row = styled.div`
+	/* margin: 15px; */
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: row;
+
+	.map {
+		width: 100%;
+		height: 500px;
 	}
 `;
 
 const Col = styled.div`
-	margin: 15px;
-	/* height: 100%; */
-	/* display: flex; */
-	/* flex-direction: column;
-	justify-content: start; */
+	width: 100%;
 `;
