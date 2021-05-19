c856191a669dd61eb5f3f3a9dd1d0ac3f2d7e944
Jeff Johnston <46493038+jjohnston55@users.noreply.github.com>
2021-05-19 11:35:00 -0600

    Created Card component for information

---
+++ src/components/Card.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import styled from "styled-components";
+
+type CardProps = {
+	title: string;
+	description: string;
+};
+
+export default function Card({ title, description }: CardProps) {
+	return (
+		<Container>
+			<h3 className="focus-in-expand">{title}</h3>
+			<h2 className="focus-in-expand">{description}</h2>
+		</Container>
+	);
+}
+
+const Container = styled.div`
+	margin: 25px 5px;
+	background-color: rgba(255, 255, 255, 0.288);
+	border-radius: 5px;
+	box-shadow: 0 0 1rem 0 rgba(0, 0, 0, 0.2);
+	padding: 0 25px;
+
+	h3 {
+		margin: 15px;
+		font-weight: 100;
+		font-size: 1.5rem;
+	}
+
+	h2 {
+		margin: 15px;
+		font-weight: 500;
+		font-size: 1.8rem;
+	}
+
+	.focus-in-expand {
+		-webkit-animation: focus-in-expand 1s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.7s both;
+		animation: focus-in-expand 1s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.7s both;
+	}
+
+	@-webkit-keyframes focus-in-expand {
+		0% {
+			letter-spacing: -0.5em;
+			-webkit-filter: blur(12px);
+			filter: blur(12px);
+			opacity: 0;
+		}
+		100% {
+			-webkit-filter: blur(0px);
+			filter: blur(0px);
+			opacity: 1;
+		}
+	}
+
+	@keyframes focus-in-expand {
+		0% {
+			letter-spacing: -0.5em;
+			-webkit-filter: blur(12px);
+			filter: blur(12px);
+			opacity: 0;
+		}
+		100% {
+			-webkit-filter: blur(0px);
+			filter: blur(0px);
+			opacity: 1;
+		}
+	}
+`;
